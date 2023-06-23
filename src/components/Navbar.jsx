07fbc53fddfef1e75aff384d19c707d94ca961fc
Samie <xxxsamie39@gmail.com>
2023-06-23 06:37:06 +0500

    nav upgradation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 mob-nav-gradient absolute top-20 right-0 mx-4 my-2 min-w-[450px] z-10 rounded-xl transition-1 `}
           >
             <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
